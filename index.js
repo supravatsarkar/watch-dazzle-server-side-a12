be0961b88d4234e6594bbab33502e5106dcf4e39
Supravat Sarkar <supravatsarkar18@gmail.com>
2021-11-13 18:35:04 +0530

    ready to test deploy

--- index.js
+++ index.js
@@ -10,9 +10,6 @@ require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 
-// DB_USER=dazzle-watch-db
-// DB_PASS=30x9XZ87gEXHNgjt
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.m4rht.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
